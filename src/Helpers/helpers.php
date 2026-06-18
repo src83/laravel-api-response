39cdd336159f075826b01f410029db879ee0c03f
Roman Staroseltsev <r.staroseltsev@gmail.com>
2026-06-18 11:20:14 +0300

    fix: move src/helpers.php to src/Helpers/helpers.php

---
+++ src/Helpers/helpers.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Log;
+use Src83\LaravelApiResponse\Exceptions\DomainLayerException;
+
+if (!function_exists('app_terminate')) {
+    /**
+     * Throw an DomainLayerException with the given error code and message.
+     *
+     * This function is used when there is no field directly associated with
+     * the error. In any other case the standard Laravel Validator must be
+     * used instead.
+     *
+     * @param string $code
+     * @param string $message
+     * @return never
+     * @throws DomainLayerException
+     */
+    function app_terminate(string $code, string $message): never
+    {
+        throw DomainLayerException::withMessages([$code => [$message]]);
+    }
+}
+
+if (!function_exists('debug_log')) {
+    /**
+     * Упрощённый логгер для отладки (только если включен debug).
+     */
+    function debug_log(mixed $data, ?string $context = null): void
+    {
+        if (config('app.debug')) {
+            Log::debug($context ? strtoupper($context) . '::' : 'DEBUG: ', (array) $data);
+        }
+    }
+}
